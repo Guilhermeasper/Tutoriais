1b5fb452619f39d2e20cbf1d4515cfae60fd0cfc
Guilherme Afonso Silva Pereira <gasp@ug4c26.windows.cin.ufpe.br>
2019-09-20 15:59:11 -0300

    Star Fied Added

--- 003 - StarField/starfield.py
+++ 003 - StarField/starfield.py
@@ -8,19 +8,22 @@ try:
 except:
     print("O modulo pygame não foi inicializado com sucesso")
 
+altura = 600
+largura = 600
+
 relogio = pygame.time.Clock()
-fundo = pygame.display.set_mode((1280,720))
-pygame.display.set_caption("Purple Rain")
+fundo = pygame.display.set_mode((altura,largura))
+pygame.display.set_caption("Star Field")
 
 class Drop:
     def __init__(self):
-        self.x = randrange(0,1280)
-        self.y = randrange(0,720)
-        self.size = randrange(3,8)
-        self.speed = self.size
+        self.x = randrange((largura/2)-50,(largura/2)+50)
+        self.y = randrange((altura/2)-50,(altura/2)+50)
+        self.radius = 1
+        self.speed = 2
 
     def show(self):
-        pygame.draw.rect(fundo, (128,0,128), [self.x, self.y, 1, self.size])
+        pygame.draw.rect(fundo, (255,255,255), tuple(self.x, self.y), self.radius)
     
     def move(self, mouse):
         self.y += self.speed
@@ -28,13 +31,8 @@ class Drop:
             self.x = randrange(0,1280)
             self.y = 0
 
-true = True
-rain = []
-for i in range(3000):
-    drop = Drop()
-    rain.append(drop)
-counter = 0
 
+true = True
 while true:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -43,19 +41,7 @@ while true:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 true = False
-    if 80 <= counter <= 83 or 86 <= counter <= 89:
-        fundo.fill((255,255,255))
-    else:
-        fundo.fill((0,0,0))
-    if counter > 89:
-        counter = 0
-    counter += 1
+    fundo.fill((0,0,0))
 
-    for drop in rain:
-        drop.show()
-        mouse = pygame.mouse.get_pos()
-        drop.move(mouse)
     pygame.display.update()
     relogio.tick(30)
-
-pygame.quit()
