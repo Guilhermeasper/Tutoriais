81657200632031bca97dd8e47f123b20688eabf5
Guilherme Afonso Silva Pereira <gasp@ug3c50.windows.cin.ufpe.br>
2019-09-27 15:40:09 -0300

    Ainda Chernoltetris

--- 004 - Tetris/tetris.py
+++ 004 - Tetris/tetris.py
@@ -12,114 +12,121 @@ except:
 altura = 500
 largura = 320
 relogio = pygame.time.Clock()
-fundo = pygame.display.set_mode((largura,altura))
+fundo = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Tetris")
 
 true = True
 
+
 def generate_random_piece():
-    pos = [[2,4],[-1,-1], [-1,-1], [-1,-1]]
+    pos = [[-1, -1], [-1, -1], [-1, -1], [-1, -1]]
 
-    piece = [[0,0,0,0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0,0,0,0],
-             [0,0,0,0,1,0,0,0,0,0],
-             [0,0,0,0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0,0,0,0]]
+    piece = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
-    frst = randrange(0,3)
+    pos[0][0] = randrange(0, 4)
+    pos[0][1] = randrange(0, 10)
+
+    piece[pos[0][0]][pos[0][1]] = 1
+
+    frst = randrange(0, 4)
 
     if frst == 0:
-        piece[1][4] = 1
-        pos[1][0] = 1
-        pos[1][1] = 4
+        piece[pos[0][0]-1][pos[0][1]] = 1
+        pos[1][0] = pos[0][0]-1
+        pos[1][1] = pos[0][1]
     elif frst == 1:
-        piece[2][5] = 1
-        pos[1][0] = 2
-        pos[1][1] = 5
+        piece[pos[0][0]][pos[0][1]+1] = 1
+        pos[1][0] = pos[0][0]
+        pos[1][1] = pos[0][1]+1
     elif frst == 2:
-        piece[2][4] = 1
-        pos[1][0] = 2
-        pos[1][1] = 4
+        piece[pos[0][0]+1][pos[0][1]] = 1
+        pos[1][0] = pos[0][0]+1
+        pos[1][1] = pos[0][1]
     else:
-        piece[2][3] = 1
-        pos[1][0] = 2
-        pos[1][1] = 3
+        piece[pos[0][0]][pos[0][1]-1] = 1
+        pos[1][0] = pos[0][0]
+        pos[1][1] = pos[0][1]-1
 
     selected = False
-    seletor = randrange(0,1)
+    seletor = randrange(0, 2)
 
     while(not selected):
-        scnd = randrange(0,3)
+        scnd = randrange(0, 4)
 
-        if scnd == 0 and piece[pos[seletor][0]-1] [pos[seletor][1]] != 1:
-            piece[pos[seletor][0]-1] [pos[seletor][1]] = 1
-            pos[2][0] = pos[seletor] [0]-1
-            pos[2][1] = pos[seletor] [1]
-            selected = true
-        elif scnd == 1 and piece[pos[seletor][0]] [pos[seletor][1]+1] != 1:
-            piece[pos[seletor][0]] [pos[seletor][1]+1] = 1
-            pos[2][0] = pos[seletor] [0]-1
-            pos[2][1] = pos[seletor] [1]
-            selected = true
-        elif scnd == 2 and piece[pos[seletor][0]+1] [pos[seletor][1]] != 1:
-            piece[pos[seletor][0]+1] [pos[seletor][1]] = 1
-            pos[2][0] = pos[seletor] [0]-1
-            pos[2][1] = pos[seletor] [1]
-            selected = true
-        elif scnd == 3 and piece[pos[seletor][0]] [pos[seletor][1]-1] != 1:
-            piece[pos[seletor][0]] [pos[seletor][1]-1] = 1
-            pos[2][0] = pos[seletor] [0]-1
-            pos[2][1] = pos[seletor] [1]
-            selected = true
-    
+        if scnd == 0 and piece[pos[seletor][0]-1][pos[seletor][1]] != 1 and pos[seletor][0]-1 >= 0:
+            piece[pos[seletor][0]-1][pos[seletor][1]] = 1
+            pos[2][0] = pos[seletor][0]-1
+            pos[2][1] = pos[seletor][1]
+            selected = True
+        elif scnd == 1 and piece[pos[seletor][0]][pos[seletor][1]+1] != 1 and pos[seletor][1]+1 <= 9:
+            piece[pos[seletor][0]][pos[seletor][1]+1] = 1
+            pos[2][0] = pos[seletor][0]
+            pos[2][1] = pos[seletor][1]+1
+            selected = True
+        elif scnd == 2 and piece[pos[seletor][0]+1][pos[seletor][1]] != 1 and pos[seletor][0]+1 <= 4:
+            piece[pos[seletor][0]+1][pos[seletor][1]] = 1
+            pos[2][0] = pos[seletor][0]+1
+            pos[2][1] = pos[seletor][1]
+            selected = True
+        elif scnd == 3 and piece[pos[seletor][0]][pos[seletor][1]-1] != 1 and pos[seletor][1]-1 >= 0:
+            piece[pos[seletor][0]][pos[seletor][1]-1] = 1
+            pos[2][0] = pos[seletor][0]
+            pos[2][1] = pos[seletor][1]-1
+            selected = True
+
     selected = False
-    seletor = randrange(0,2)
+    seletor = randrange(0, 3)
 
     while(not selected):
-        scnd = randrange(0,3)
+        scnd = randrange(0, 4)
 
-        if scnd == 0 and piece[pos[seletor][0]-1] [pos[seletor][1]] != 1:
-            piece[pos[seletor][0]-1] [pos[seletor][1]] = 1
-            pos[2][0] = pos[seletor] [0]-1
-            pos[2][1] = pos[seletor] [1]
-            selected = true
-        elif scnd == 1 and piece[pos[seletor][0]] [pos[seletor][1]+1] != 1:
-            piece[pos[seletor][0]] [pos[seletor][1]+1] = 1
-            pos[2][0] = pos[seletor] [0]-1
-            pos[2][1] = pos[seletor] [1]
-            selected = true
-        elif scnd == 2 and piece[pos[seletor][0]+1] [pos[seletor][1]] != 1:
-            piece[pos[seletor][0]+1] [pos[seletor][1]] = 1
-            pos[2][0] = pos[seletor] [0]-1
-            pos[2][1] = pos[seletor] [1]
-            selected = true
-        elif scnd == 3 and piece[pos[seletor][0]] [pos[seletor][1]-1] != 1:
-            piece[pos[seletor][0]] [pos[seletor][1]-1] = 1
-            pos[2][0] = pos[seletor] [0]-1
-            pos[2][1] = pos[seletor] [1]
-            selected = true
+        if scnd == 0 and piece[pos[seletor][0]-1][pos[seletor][1]] != 1 and pos[seletor][0]-1 >= 0:
+            piece[pos[seletor][0]-1][pos[seletor][1]] = 1
+            pos[2][0] = pos[seletor][0]-1
+            pos[2][1] = pos[seletor][1]
+            selected = True
+        elif scnd == 1 and piece[pos[seletor][0]][pos[seletor][1]+1] != 1 and pos[seletor][1]+1 <= 9:
+            piece[pos[seletor][0]][pos[seletor][1]+1] = 1
+            pos[2][0] = pos[seletor][0]
+            pos[2][1] = pos[seletor][1]+1
+            selected = True
+        elif scnd == 2 and piece[pos[seletor][0]+1][pos[seletor][1]] != 1 and pos[seletor][0]+1 <= 15:
+            piece[pos[seletor][0]+1][pos[seletor][1]] = 1
+            pos[2][0] = pos[seletor][0]+1
+            pos[2][1] = pos[seletor][1]
+            selected = True
+        elif scnd == 3 and piece[pos[seletor][0]][pos[seletor][1]-1] != 1 and pos[seletor][1]-1 >= 0:
+            piece[pos[seletor][0]][pos[seletor][1]-1] = 1
+            pos[2][0] = pos[seletor][0]
+            pos[2][1] = pos[seletor][1]-1
+            selected = True
 
     for line in piece:
         print(line)
     print("")
+    return piece
 
 
-grid = [[0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0]]
+grid = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
 while true:
     for event in pygame.event.get():
@@ -128,7 +135,10 @@ while true:
             break
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                generate_random_piece()
+                new = generate_random_piece()
+            for i in range(5):
+                for j in range(10):
+                    grid[i+1][j] = new[i][j]
             if event.key == pygame.K_UP:
                 for i in range(16):
                     for j in range(10):
@@ -136,8 +146,8 @@ while true:
                             grid[i][j] = 0
                             grid[i-1][j] = 1
             if event.key == pygame.K_DOWN:
-                for i in range(15,0,-1):
-                    for j in range(9,0,-1):
+                for i in range(15, 0, -1):
+                    for j in range(9, 0, -1):
                         if grid[i][j] == 1:
                             grid[i][j] = 0
                             grid[i+1][j] = 1
@@ -148,33 +158,30 @@ while true:
                             grid[i][j] = 0
                             grid[i][j-1] = 1
             if event.key == pygame.K_RIGHT:
-                for j in range(9,0,-1):
+                for j in range(9, 0, -1):
                     for i in range(16):
                         if grid[i][j] == 1:
                             grid[i][j] = 0
                             grid[i][j+1] = 1
-    fundo.fill((120,120,120))
+    fundo.fill((120, 120, 120))
     for i in range(16):
         for j in range(10):
             if grid[i][j] == 1 or grid[i][j] == 2:
-                for x in range(1,28):
-                    for y in range(1,28):
-                        fundo.set_at((j*30+x, i*30+y), (0,255,255))
-        for i in range(15,0,-1):
-            for j in range(9,0,-1):
+                for x in range(1, 28):
+                    for y in range(1, 28):
+                        fundo.set_at((j*30+x, i*30+y), (0, 255, 255))
+        for i in range(15, 0, -1):
+            for j in range(9, 0, -1):
                 if i == 15 and grid[i][j] == 1:
                     for i in range(16):
                         for j in range(10):
                             if grid[i][j] == 1:
                                 grid[i][j] = 2
-    for i in range(15,0,-1):
-        for j in range(9,0,-1):
+    for i in range(15, 0, -1):
+        for j in range(9, 0, -1):
             if grid[i][j] == 1:
                 grid[i][j] = 0
                 grid[i+1][j] = 1
-                
-    
-
     
     pygame.display.update()
     relogio.tick(5)
