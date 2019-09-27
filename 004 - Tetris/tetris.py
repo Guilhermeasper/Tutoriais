f80121ed4d63bebd72e634be269b6f6216abe246
Guilherme Afonso Silva Pereira <gasp@ug3c39.windows.cin.ufpe.br>
2019-09-27 12:12:36 -0300

    Evolui no tetris

--- 004 - Tetris/tetris.py
+++ 004 - Tetris/tetris.py
@@ -16,9 +16,32 @@ fundo = pygame.display.set_mode((largura,altura))
 pygame.display.set_caption("Tetris")
 
 true = True
+
+def generate_random_piece():
+    pos = [[2,4],[-1,-1], [-1,-1], [-1,-1]]
+
+    piece = [[0,0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,1,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0,0]]
+
+    frst = randrange(0,3)
+
+    if frst == 0:
+        piece[1][4] = 1
+    elif frst == 1:
+        piece[2][5] = 1
+    elif frst == 2:
+        piece[2][4] = 1
+    else:
+        piece[2][3] = 1
+
+
+
 grid = [[0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,1,1,0,0],
-        [0,0,0,0,0,1,1,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0],
@@ -46,35 +69,49 @@ while true:
                     for j in range(10):
                         if grid[i][j] == 1:
                             grid[i][j] = 0
-                            grid[i][j-1] = 1
-            if event.key == pygame.K_DOWN:
-                for i in range(16):
-                    for j in range(10):
-                        if grid[i][j] == 1:
-                            grid[i][j] = 0
-                            grid[i][j+1] = 1
-            if event.key == pygame.K_LEFT:
-                for i in range(16):
-                    for j in range(10):
-                        if grid[i][j] == 1:
-                            grid[i][j] = 0
                             grid[i-1][j] = 1
-            if event.key == pygame.K_RIGHT:
-                for i in range(16):
-                    for j in range(10):
+            if event.key == pygame.K_DOWN:
+                for i in range(15,0,-1):
+                    for j in range(9,0,-1):
                         if grid[i][j] == 1:
                             grid[i][j] = 0
                             grid[i+1][j] = 1
-    fundo.fill((0,0,0))
+            if event.key == pygame.K_LEFT:
+                for j in range(10):
+                    for i in range(16):
+                        if grid[i][j] == 1:
+                            grid[i][j] = 0
+                            grid[i][j-1] = 1
+            if event.key == pygame.K_RIGHT:
+                for j in range(9,0,-1):
+                    for i in range(16):
+                        if grid[i][j] == 1:
+                            grid[i][j] = 0
+                            grid[i][j+1] = 1
+    fundo.fill((120,120,120))
     for i in range(16):
         for j in range(10):
+            if grid[i][j] == 1 or grid[i][j] == 2:
+                for x in range(1,28):
+                    for y in range(1,28):
+                        fundo.set_at((j*30+x, i*30+y), (0,255,255))
+        for i in range(15,0,-1):
+            for j in range(9,0,-1):
+                if i == 15 and grid[i][j] == 1:
+                    for i in range(16):
+                        for j in range(10):
+                            if grid[i][j] == 1:
+                                grid[i][j] = 2
+    for i in range(15,0,-1):
+        for j in range(9,0,-1):
             if grid[i][j] == 1:
-                for x in range(30):
-                    for y in range(30):
-                        fundo.set_at((j*30+x, i*30+y), (255,255,255))
+                grid[i][j] = 0
+                grid[i+1][j] = 1
+                
+    
 
     
     pygame.display.update()
-    relogio.tick(30)
+    relogio.tick(5)
 
 pygame.quit()
